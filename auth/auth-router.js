c36fcfefb03f185140e540c95f82e4d5662424f2
Alex <agordon1123@gmail.com>
2019-09-18 12:25:22 -0400

    created JWT response

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -18,16 +19,16 @@ router.post('/register', (req, res) => {
     });
 });
 
+// Typical practice is to send the token at register to log them in /|\
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
+      const token = generateToken(user);
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-        });
+        res.status(200).json({ token });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
@@ -36,5 +37,16 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const secret = 'keep it secret, keep it safe!';
+  const options = {
+    expiresIn: '1h'
+  }
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
